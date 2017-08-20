a316a7890fcdf79f89d73df699d39d1505c2de49
Akuli <akuviljanen17@gmail.com>
2017-08-20 22:36:34 +0300

    i think i fixed it

--- tk_markup/_create_widgets.py
+++ tk_markup/_create_widgets.py
@@ -20,17 +20,16 @@ def create_widget(parent_widget, class_name, pack_opts, grid_opts,
     if parent_widget is None:
         # probably Tk
         widget = cls()
-        widget.config(**config)
+        widget.config(**_to_dict(widget, config_opts))
     else:
-        config = dict(_pairs(parent_widget.tk.splitlist(config_opts)))
-        widget = cls(parent_widget, **config)
+        widget = cls(parent_widget, **_to_dict(parent_widget, config))
 
     # tkinter raises an error if more than one of these is specified
     if pack_opts is not None:
-        widget.pack(**_to_dict(pack_opts))
+        widget.pack(**_to_dict(widget, pack_opts))
     if grid_opts is not None:
-        widget.grid(**_to_dict(grid_opts))
+        widget.grid(**_to_dict(widget, grid_opts))
     if place_opts is not None:
-        widget.place(**_to_dict(place_opts)
+        widget.place(**_to_dict(widget, place_opts)
 
     return widget
